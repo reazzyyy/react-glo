f13111c3335e10d90d20e93452dd3401187e1c9a
Semion Mudrov <mudrsemen@ya.ru>
2025-03-25 22:00:48 +0300

    Feat: lesson10 immutable state handleSubmit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,81 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
-
+import nextId from 'react-id-generator';
 import './app.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          name: 'John C.',
+          salary: 800,
+          increase: false,
+          like: false,
+          id: nextId(),
+        },
+        {
+          name: 'Alex M.',
+          salary: 3000,
+          increase: true,
+          like: false,
+          id: nextId(),
+        },
+        {
+          name: 'Carl W.',
+          salary: 5000,
+          increase: false,
+          like: true,
+          id: nextId(),
+        },
+      ],
+    };
+  }
 
-function App() {
-  const data = [
-    { name: 'John C#', salary: '800', increase: true, id: 1 },
-    { name: 'Alex Bb', salary: '3000', increase: false, id: 2 },
-    { name: 'Bob M', salary: '5000', increase: false, id: 3 },
-  ];
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-  return (
-    <div className="app">
-      <AppInfo />
+  createEmploee = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      like: false,
+      id: nextId(),
+    };
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployeesAddForm onCreate={this.createEmploee} />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
